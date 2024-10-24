78d875d68c42420d04d9c20d7e3229254846b036
SanyaShpot <151662576+SanyaShpot@users.noreply.github.com>
2024-10-24 22:24:36 +0300

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "root",
+    DATABASE: "Trees",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
